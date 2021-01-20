c0f21bb33ff9b91f142ad4f75bd132958fa4469a
Pascal De Poorter <pascal.de.poorter@cubis.be>
2021-01-20 12:14:04 +0100

    remove debugging info + corrections

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ This Mod will copy the content of a blueprint into the Spidertron logistics slot
 
 How it works:
 1. Place a blueprint in the Spidertron trunk. Currently only the first blueprint is picked-up ( still have to figure out if I can cycle throught the blueprints placed in the Trunk). The blueprints entities will be added to quantities already requested in the slots.
-2. Issue follwing naming conventions:
+2. Issue following naming conventions:
    - The copy/addtion of the blueprint is only triggered if the blueprint's label starts with \_P\_
    - Include \_E\_ in the label in case you first want to clear the logistics slots
-   - Include \_n_x_\_ x is a multiplier to request entities for multiple blueprint stamps.
+   - Include \_nx_\_ x is a multiplier to request x multiple of entities from the blueprint.
 3. Apply the blueprint by moving the Spidertron using its remote control.
 4. after treamtment the first underscore is removed to prevent further treatment.
--- control.lua
+++ control.lua
@@ -1,26 +1,11 @@
 -- Mod: bluespiderMod
 
--- Aggregate all unique item/entities in the blueprint by item
-function aggregate_entities(ents)
-    itemList = {}
-    for _, ent in pairs(ents) do
-        if (itemList[ent.name] == nil) then
-            itemList[ent.name] = 1
-        else
-            itemList[ent.name] = itemList[ent.name] + 1
-        end
-    end
-    return itemList
-end
-
 -- Copy the items in the blueprint to the logistic slot.
 function set_logistic_request(veh, item, count, slot, numberOfCopies)
-    game.print("Item adding " ..  slot)
     itemStack = {} 
     itemStack["name"] = item
     itemStack["count"] = count * numberOfCopies
     veh.set_request_slot(itemStack, slot)
-    game.print("Item added")
 end
 
 -- Emtpy the logistics grid
@@ -32,43 +17,33 @@ end
 
 -- add the blueprint items to the grid, add in case of already existing items.
 function add_bp_to_logistics_grid(veh, bp, numberOfCopies)
-    items = aggregate_entities(bp.get_blueprint_entities())
+--   0.0.2 -- issues with difference in entity and item name causing dumps. found cost_to_build dictionary
+--   items = aggregate_entities(bp.get_blueprint_entities())
+    items = bp.cost_to_build
     if (items ~= nil) then
         i = 0
         nextSlot = veh.request_slot_count + 1
         lastUsedSlot = veh.request_slot_count
         for k, v in pairs(items) do
             i_found = false
-            game.print("Run over the grid for " .. k)
-            game.print(veh.request_slot_count)
             for i=1, lastUsedSlot do
-               game.print("Start Looping")
                slot_item = veh.get_request_slot(i)
                if slot_item ~= nil then           
-                game.print(slot_item.name .. ":" .. k)
                     if (slot_item.name == k) then
                         i_found = true
-                        useSlot = i
-                        game.print("Break Looping")                       
+                        useSlot = i                    
                         break
                     end
                end
-               game.print("End Looping")
             end
-            game.print("Decide what to do with the item")
             if i_found then
-                game.print("Found")
                 v = v + slot_item.count
             else
-                game.print("Not Found")
-                game.print(veh.request_slot_count)
-                game.print(i_favail)
                 useSlot = nextSlot
                 nextSlot = nextSlot + 1
             end
             set_logistic_request(veh, k, v, useSlot, numberOfCopies)
         end
-        game.print("Zouden we hier al gepasseerd zijn")
     end
 end
 
@@ -97,7 +72,6 @@ function spider_remote_used(event)
             end 
         end
     end
-    game.print("Testing")
 end
 
 -- Capture the event when the spidertron remote is used
